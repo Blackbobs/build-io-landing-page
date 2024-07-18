97ba06dd54589cb74c640e4b06e187c4ad0f4c22
Black Bobs <ayodejiayodele350@gmail.com>
2024-07-18 01:06:49 +0100

    thinking of a way to setup waitlist

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -19,16 +19,21 @@ const index: React.FC = () => {
             start building today
           </a>
         </button>
-        <div className="my-3 text-center w-full flex flex-col items-center">
+        {/* <div className="my-3 text-center w-full flex flex-col items-center">
           <small className="m-2 capitalize font-medium text-[14px] inline-block">join our waitlist</small>
           <form className="flex gap-1">
           <input className="bg-primary text-[#1a1a1a] p-2 outline-none rounded focus:border-brand focus:border-2" type="text" placeholder="enter your email..." autoComplete="false" />
           <button className="bg-brand p-2 outline-none rounded md:px-3" type="submit">Join waitlist</button>
           </form>
-        </div>
+        </div> */}
       </div>
     </section>
   );
 };
+
+// https://app.us14.list-manage.com/subscribe/post
+// 352baf2487fb1f29cc5a617c5
+// a2f32abab7
+// MERGE0
 
 export default index;
